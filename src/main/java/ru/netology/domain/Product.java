fb86614007b6ee7fdee8c61bf1a9fea974c66319
NastyaImp <Imperiian@yandex.ru>
2021-12-16 20:50:31 +0300

    manager

---
+++ src/main/java/ru/netology/domain/Product.java
@@ -0,0 +1,17 @@
+package ru.netology.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+import java.util.Objects;
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+@Data
+public class Product {
+    private int id;
+    private String name;
+    private int price;
+}
